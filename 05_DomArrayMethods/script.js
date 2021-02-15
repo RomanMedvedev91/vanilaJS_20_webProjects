63e41ac3a9ab1356d529ce69ff4f26f13b49491f
Roman Medvedev <roman.medvedev77@gmail.com>
2021-02-15 18:42:25 +0500

    feat: implement task5

--- 05_DomArrayMethods/script.js
+++ 05_DomArrayMethods/script.js
@@ -5,16 +5,7 @@ const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
 let data = [];
-getRandomUser();
-getRandomUser();
-getRandomUser();
 
-// fetch random user and add money
-// function getRandomUser() {
-//     fetch('https://randomuser.me/api')
-//     .then(res => res.json())
-//     .then(data => console.log(data));
-// }
 async function getRandomUser() {
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
@@ -29,6 +20,41 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+// Double Money
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+}
+
+// sort users by richest
+function sortByRichest() {
+  data.sort((a, b) => {
+    return b.money - a.money;
+  });
+  updateDOM();
+}
+
+// filter only MIlioneres
+function showMillionaires() {
+  data = data.filter((user) => {
+    return user.money > 1000000;
+  });
+  updateDOM();
+}
+
+// calculateWealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => {
+    return (acc += user.money);
+  }, 0);
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth:<strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
 // add new obj to data arr
 
 function addData(obj) {
@@ -58,3 +84,7 @@ function formatMoney(number) {
 
 // event listener
 addUserBtn.addEventListener("click", getRandomUser);
+doubleBtn.addEventListener("click", doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
